0d014fba663da815b1f2a02c958e5dcf20e9cb14
Kevin Jiang <kevin51jiang@email.com>
2020-05-01 00:54:20 -0400

    Updates

---
+++ precache-manifest.3af785ffbb8b8c64e80e36b147ac3515.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc10fa8a6aa346a872b191f053c3cb1",
+    "url": "/Spotify-Web-Player/index.html"
+  },
+  {
+    "revision": "e3fd1406bb05af34b76f",
+    "url": "/Spotify-Web-Player/static/css/main.2f8462ba.chunk.css"
+  },
+  {
+    "revision": "8b53ccb863edcc68fcf8",
+    "url": "/Spotify-Web-Player/static/js/2.344def27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Spotify-Web-Player/static/js/2.344def27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3fd1406bb05af34b76f",
+    "url": "/Spotify-Web-Player/static/js/main.5b644417.chunk.js"
+  },
+  {
+    "revision": "3092350a6f64aa9e6fa5",
+    "url": "/Spotify-Web-Player/static/js/runtime-main.a3a3ef0e.js"
+  },
+  {
+    "revision": "6264d8cb7acff2b434c52044518994d3",
+    "url": "/Spotify-Web-Player/static/media/record.6264d8cb.webp"
+  }
+]);
